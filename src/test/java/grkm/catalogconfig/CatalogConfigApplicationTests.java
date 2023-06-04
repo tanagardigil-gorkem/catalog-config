9a43fcb8fd9b43fbd0a1df95d18a1ac7eb0c623e
Gorkem Tanagardigil <119583820+tanagardigil-gorkem@users.noreply.github.com>
2023-06-04 22:45:25 +0200

    first commit

---
+++ src/test/java/grkm/catalogconfig/CatalogConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package grkm.catalogconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
